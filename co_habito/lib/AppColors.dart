894ff917d03408a35f2b71ecc72f2aa63adef19e
João Gustavo Medeiros Pontes <joaogustavomp.jgm@gmail.com>
2024-09-15 21:22:13 -0300

    Add fonts and colors

---
+++ co_habito/lib/AppColors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color Lapis_Lazuli = Color.fromARGB(255, 51, 102, 153);
+  static const Color Carolina_Blue = Color.fromARGB(255, 134, 187, 216);
+  static const Color Charcoal = Color.fromARGB(255, 47, 72, 88);
+  static const Color Light_Green = Color.fromARGB(255, 158, 228, 147);
+  static const Color Nyanza = Color.fromARGB(255, 218, 247, 220);
+}
+
+// Para utilizar essas cores em outras partes do código, basta importar o arquivo AppColors.dart e fazer referência às cores.
+
+// import 'AppColors.dart';
+
+// AppColors.Lapis_Lazuli
+// AppColors.Carolina_Blue
+// AppColors.Charcoal
+// AppColors.Light_Green
+// AppColors.Nyanza
