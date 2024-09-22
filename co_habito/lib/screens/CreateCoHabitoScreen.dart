92354b55b7ca233f5ec7c9b684738639ec3df1be
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-22 17:21:36 -0300

    Tela de criação de cohabito

--- co_habito/lib/screens/CreateCoHabitoScreen.dart
+++ co_habito/lib/screens/CreateCoHabitoScreen.dart
@@ -1,7 +1,16 @@
 import 'package:co_habito/AppColors.dart';
 import 'package:co_habito/components/BodyWithSmallHeader.dart';
+import 'package:co_habito/components/CustomImageButton.dart';
+import 'package:co_habito/components/GenericalButton.dart';
 import 'package:co_habito/components/HeaderTitle.dart';
 import 'package:co_habito/components/SmallHeader.dart';
+import 'package:co_habito/screens/CoHabitoScreen.dart';
+import 'package:co_habito/screens/SelectCoHabitoScreen.dart';
+import 'package:co_habito/screens/SelectTasksScreen.dart';
+import 'package:co_habito/screens/StartScreen.dart';
+import 'package:co_habito/tools/Go.dart';
+import 'package:co_habito/components/GenericalIput.dart';
+import 'package:co_habito/components/InputLabel.dart';
 import 'package:flutter/material.dart';
 
 class CreateCoHabitoScreen extends StatelessWidget{
@@ -9,14 +18,23 @@ class CreateCoHabitoScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: AppColors.Carolina_Blue,
-      appBar: SmallHeader(title: HeaderTitle(title: "Criar CoHábito", fontSize: 32),),
+      appBar: SmallHeader(
+        before: CustomImageButton(imageUrl: "", onPressed: (){Go.to(SelectCoHabitoScreen(), context);}),
+        title: HeaderTitle(title: "Criar CoHábito", fontSize: 32),
+        after: CustomImageButton(imageUrl: "", onPressed: (){Go.to(StartScreen(), context);}),
+      ),
       body: BodyWithSmallHeader(
         child: Center(
           child: Container(
             width: 308,
             child: Column(
               children: [
-                
+                SizedBox(height: 60,),
+                Align(alignment: Alignment.centerLeft, child: InputLabel(string: "Nome:", fontSize: 14)),
+                SizedBox(height: 10,),
+                GenericalInput(textInputType: TextInputType.text, ispassword: false),
+                SizedBox(height: 40,),
+                GenericalButton(buttonText: "Criar", onPressed: (){Go.to(SelectTasksScreen(), context);})
               ],
             ),
           )
