33e4845aaf0efe0add5e19663c6562b6c99627d6
Smartucheli <savio.martucheli@gmail.com>
2024-09-22 11:08:54 -0300

    Adicionando os componentes

---
+++ co_habito/lib/components/inputpesquisa.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:co_habito/AppColors.dart';
+
+class SearchInput extends StatelessWidget {
+  final TextInputType textInputType;
+  final TextEditingController controller;
+
+  SearchInput({
+    required this.textInputType,
+    TextEditingController? controller,
+  }) : this.controller = controller ?? TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 362,
+      height: 56, // Ajuste a altura para melhor visualização
+      child: TextField(
+        controller: controller,
+        keyboardType: textInputType,
+        cursorColor: Color.fromARGB(255, 95, 95, 95),
+        decoration: InputDecoration(
+          hintText: 'Pesquisar ...', // Placeholder
+          hintStyle: TextStyle(color: Color.fromARGB(255, 131, 131, 131)), // Estilo do placeholder
+          fillColor: Color(0xFFF6F6F6),
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: const BorderSide(
+              color: Color(0xFFB3B3B3),
+              width: 1.5,
+            ),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            borderSide: const BorderSide(
+              color: Color(0xFFB3B3B3),
+              width: 1.5,
+            ),
+          ),
+          suffixIcon: Icon(
+            Icons.search,
+            color: Colors.blue, // Cor do ícone
+          ),
+        ),
+      ),
+    );
+  }
+}
--- co_habito/lib/main.dart
+++ co_habito/lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 import 'AppColors.dart';
 import 'components/GenericalIput.dart';
+import 'components/TarefaTempoDificuldade.dart';
+import 'components/CardTask.dart';
+import 'components/inputpesquisa.dart';
+import 'components/CardCohabito.dart';
+import 'components/guiapessoas.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      body: Center(
+      body: SingleChildScrollView(
           child: Column(
             children: [
-              GenericalInput(
-                textInputType: TextInputType.number,
-                ispassword: false,
-              ),
+              ParticipantList(participantes: [
+              'João',
+              'Pedro (3)',
+              'Caio',
+              'Vitor',
+              'Ana',
+              'Maria',
+              'Lucas',
+              'Carla',
+              ]),
             ],
           ),
         ),
