8b9a1565a63c4adb2e24f263a1d890a350a18fde
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-22 15:12:34 -0300

    Tela inicial estática

--- co_habito/lib/screens/StartScreen.dart
+++ co_habito/lib/screens/StartScreen.dart
@@ -1,7 +1,9 @@
 import 'package:co_habito/AppColors.dart';
 import 'package:co_habito/components/BodyWithLargeHeader.dart';
+import 'package:co_habito/components/GenericalButton.dart';
 import 'package:co_habito/components/HeaderTitle.dart';
 import 'package:co_habito/components/LargeHeader.dart';
+import 'package:co_habito/components/OULabel.dart';
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget{
@@ -15,7 +17,10 @@ class StartScreen extends StatelessWidget{
           width: 241,
           child: Column(
             children: [
-              
+              SizedBox(height: 40,),
+              GenericalButton(buttonText: "Login", onPressed: (){}),
+              OULabel(),
+              GenericalButton(buttonText: "Cadastrar", onPressed: (){})
             ],
           ),
         ),
