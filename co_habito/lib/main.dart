7ba5e3733355643505b1f7b010cf59bc341bcd42
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-21 23:05:21 -0300

    AppBar para body com header pequeno

--- co_habito/lib/main.dart
+++ co_habito/lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:co_habito/components/BodyWithMediumHeader.dart';
-import 'package:co_habito/components/MediumHeader.dart';
+import 'package:co_habito/components/BodyWithSmallHeader.dart';
+import 'package:co_habito/components/HeaderTitle.dart';
+import 'package:co_habito/components/SmallHeader.dart';
 import 'package:flutter/material.dart';
 import 'AppColors.dart';
 
@@ -7,8 +8,8 @@ void main() {
   runApp( MaterialApp(
     home: Scaffold(
       backgroundColor: AppColors.Carolina_Blue,
-      appBar: MediumHeader(),
-      body: BodyWithMediumHeader(child: Column(children: [FloatingActionButton(onPressed: null)])),
+      appBar: SmallHeader(before: FloatingActionButton(onPressed: null), title: HeaderTitle(title: "Selecionar tarefas", fontSize: 26), after: FloatingActionButton(onPressed: null)),
+      body: BodyWithSmallHeader(child: Column(children: [FloatingActionButton(onPressed: null)])),
     ),
   ));
 }
