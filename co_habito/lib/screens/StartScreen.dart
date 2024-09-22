b8d4f13e7ec965bb42e311bad58fa722dd26dfa9
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-22 15:29:31 -0300

    Cria o Go.to para facilitar a navegação

--- co_habito/lib/screens/StartScreen.dart
+++ co_habito/lib/screens/StartScreen.dart
@@ -4,6 +4,9 @@ import 'package:co_habito/components/GenericalButton.dart';
 import 'package:co_habito/components/HeaderTitle.dart';
 import 'package:co_habito/components/LargeHeader.dart';
 import 'package:co_habito/components/OULabel.dart';
+import 'package:co_habito/screens/LoginScreen.dart';
+import 'package:co_habito/screens/registerScreen.dart';
+import 'package:co_habito/tools/Go.dart';
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget{
@@ -18,9 +21,13 @@ class StartScreen extends StatelessWidget{
           child: Column(
             children: [
               SizedBox(height: 40,),
-              GenericalButton(buttonText: "Login", onPressed: (){}),
+              GenericalButton(buttonText: "Login", onPressed: (){
+                Go.to(LoginScreen(), context);
+              }),
               OULabel(),
-              GenericalButton(buttonText: "Cadastrar", onPressed: (){})
+              GenericalButton(buttonText: "Cadastrar", onPressed: (){
+                Go.to(RegisterScreen(), context);
+              })
             ],
           ),
         ),
