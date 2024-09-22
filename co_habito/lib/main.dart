8abf9d912f4c3e485d973072953b127634665d49
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-21 22:46:51 -0300

    AppBar para o body com header médio

--- co_habito/lib/main.dart
+++ co_habito/lib/main.dart
@@ -1,6 +1,5 @@
-import 'package:co_habito/components/BodyWithLargeHeader.dart';
-import 'package:co_habito/components/HeaderTitle.dart';
-import 'package:co_habito/components/LargeHeader.dart';
+import 'package:co_habito/components/BodyWithMediumHeader.dart';
+import 'package:co_habito/components/MediumHeader.dart';
 import 'package:flutter/material.dart';
 import 'AppColors.dart';
 
@@ -8,8 +7,8 @@ void main() {
   runApp( MaterialApp(
     home: Scaffold(
       backgroundColor: AppColors.Carolina_Blue,
-      appBar: LargeHeader(title: HeaderTitle(title: "CoHábito", fontSize: 40),),
-      body: BodyWithLargeHeader(child: Column(children: [FloatingActionButton(onPressed: null)])),
+      appBar: MediumHeader(),
+      body: BodyWithMediumHeader(child: Column(children: [FloatingActionButton(onPressed: null)])),
     ),
   ));
 }
