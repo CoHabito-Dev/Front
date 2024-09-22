dd1df0947421c829e8fb4e928205ee6809d82b02
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-22 17:51:35 -0300

    Tela de entrar em cohábito

--- co_habito/lib/screens/EnterCoHabitoScreen.dart
+++ co_habito/lib/screens/EnterCoHabitoScreen.dart
@@ -2,10 +2,14 @@ import 'package:co_habito/AppColors.dart';
 import 'package:co_habito/components/BodyWithSmallHeader.dart';
 import 'package:co_habito/components/HeaderTitle.dart';
 import 'package:co_habito/components/SmallHeader.dart';
+import 'package:co_habito/screens/CoHabitoScreen.dart';
 import 'package:co_habito/screens/SelectCoHabitoScreen.dart';
 import 'package:co_habito/tools/Go.dart';
 import 'package:co_habito/components/CustomImageButton.dart';
 import 'package:co_habito/screens/StartScreen.dart';
+import 'package:co_habito/components/GenericalIput.dart';
+import 'package:co_habito/components/GenericalButton.dart';
+import 'package:co_habito/components/InputLabel.dart';
 import 'package:flutter/material.dart';
 
 class EnterCoHabitoScreen extends StatelessWidget{
@@ -24,7 +28,12 @@ class EnterCoHabitoScreen extends StatelessWidget{
             width: 308,
             child: Column(
               children: [
-                
+                SizedBox(height: 60,),
+                Align(alignment: Alignment.centerLeft, child: InputLabel(string: "Insira o ID do CoHábito que deseja entrar:", fontSize: 14)),
+                SizedBox(height: 10,),
+                GenericalInput(textInputType: TextInputType.text, ispassword: false),
+                SizedBox(height: 40,),
+                GenericalButton(buttonText: "Acessar", onPressed: (){Go.to(CoHabitoScreen(coHabitoName: "Casa dos Pais"), context);})
               ],
             ),
           )
