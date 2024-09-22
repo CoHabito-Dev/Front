8d302baa17713d095a7502dca46395bed37297b3
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-22 14:24:52 -0300

    Adiciona o Scaffold a todas as telas

---
+++ co_habito/lib/screens/CreateCoHabitoScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateCoHabitoScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
--- co_habito/lib/screens/StartScreen.dart
+++ co_habito/lib/screens/StartScreen.dart
@@ -4,7 +4,7 @@ class StartScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      
     );
   }
 }
