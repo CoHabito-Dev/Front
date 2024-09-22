d61752e01e95988ea11fc4d93f691e99d02e7491
Caio Fernandes <caiovitfernandes@gmail.com>
2024-09-22 14:27:17 -0300

    Adiciona o background color a todas as telas

--- co_habito/lib/screens/SelectTasksScreen.dart
+++ co_habito/lib/screens/SelectTasksScreen.dart
@@ -1,10 +1,11 @@
+import 'package:co_habito/AppColors.dart';
 import 'package:flutter/material.dart';
 
 class SelectTasksScreen extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      backgroundColor: AppColors.Carolina_Blue,
     );
   }
 }
